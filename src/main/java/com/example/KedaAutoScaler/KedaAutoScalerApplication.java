84e3c5687a40a9c0011ce0e1660be9f19dbaade1
simrankhanna <simrankhanna0.17@gmail.com>
2024-04-21 21:23:54 +0530

    feat(kedaAutoScaler): add starter code

---
+++ src/main/java/com/example/KedaAutoScaler/KedaAutoScalerApplication.java
@@ -0,0 +1,13 @@
+package com.example.KedaAutoScaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KedaAutoScalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KedaAutoScalerApplication.class, args);
+	}
+
+}
